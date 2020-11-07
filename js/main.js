8de842d213c710545d1f4f35b1b0ec96edeba31a
moonbl4 <gmoonbl4@gmail.com>
2020-11-07 15:20:08 +0300

    slaider finish

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function(){
 
-    $('.slaider_inner').slick();    
+    $('.slaider_inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"><i class="far fa-arrow-alt-circle-right"></i></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="far fa-arrow-alt-circle-left"></i></button>',
+    });    
 
 
 
