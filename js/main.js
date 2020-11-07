fe3bff0df41110efbad5953403dae888cfae6da4
moonbl4 <gmoonbl4@gmail.com>
2020-11-07 17:32:01 +0300

    change header top

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
 
     $('.slaider_inner').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next"><i class="far fa-arrow-alt-circle-right"></i></button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="far fa-arrow-alt-circle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><i class="fas fa-arrow-alt-circle-right"></i></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="fas fa-arrow-alt-circle-left"></i></button>',
     });    
 
 
